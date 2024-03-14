748564c5a299c4726cf7c3cca247209ac3c2ddd4
Tomasiko04 <tomasbajer04@seznam.cz>
2024-03-14 23:40:21 +0100

    12

---
+++ filtr.py
@@ -0,0 +1,31 @@
+"pip install pillow"
+#from PIL import Image
+
+#brazek = Image.open("2.jpg")
+obrazek=0
+sirka, vyska = obrazek.size
+x = 0
+
+while x < sirka:
+    y = 0
+    while y < vyska:
+        r, g, b = obrazek.getpixel((x, y))
+        prumer = int((r + g + b) / 3)
+        obrazek.putpixel((x, y), (r, b, r))
+
+        if prumer > 200:
+            obrazek.putpixel((x, y), (255, 150, 255))  # Bílá pro světlé pixely
+        elif 160 < prumer <= 200:
+            obrazek.putpixel((x, y), (240, 210, 190))  # Teplý odstín s vyšší intenzitou
+        elif 120 < prumer <= 160:
+            obrazek.putpixel((x, y), (100, 200, 240))  # Chladný odstín s vyšší intenzitou
+        elif 80 < prumer <= 120:
+            obrazek.putpixel((x, y), (100, 130, 160))  # Střední intenzita šedé
+
+        elif prumer <= 40:
+            obrazek.putpixel((x, y), (140, 30, 30))      # Mírně do červena s vyšší intenzitou pro tmavé pixely
+
+        y += 1
+    x += 1
+
+obrazek.show()#display(obrazek)
